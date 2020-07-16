6c1dbea6c824cdd644bba0ee3881ba68c4ead3e5
Le Ba Hoang An <hoanganleba@gmail.com>
2020-07-17 00:55:54 +0700

    Add postgresql

--- src/main/java/com/example/onlinebookingsystem/model/Contact.java
+++ src/main/java/com/example/onlinebookingsystem/model/Contact.java
@@ -1,25 +1,53 @@
 package com.example.onlinebookingsystem.model;
 
-import lombok.Data;
+import javax.persistence.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-
-@Data
 @Entity
-
+@Table(name="contacts")
 public class Contact {
-    private @Id @GeneratedValue Long id;
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name="first_name")
     private String firstName;
+
+    @Column(name="last_name")
     private String lastName;
+
+    @Column(name="email")
     private String email;
 
-    private Contact() {}
+    public Contact() {}
 
     public Contact(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
     }
 }
--- src/main/java/com/example/onlinebookingsystem/repository/ContactRepository.java
+++ src/main/java/com/example/onlinebookingsystem/repository/ContactRepository.java
@@ -2,6 +2,8 @@ package com.example.onlinebookingsystem.repository;
 
 import com.example.onlinebookingsystem.model.Contact;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ContactRepository extends CrudRepository<Contact, Long> {
 }
